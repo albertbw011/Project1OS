c78783b334e5deec51742fea9222d63e480cdf13
Albert Wang <albertbw011@utexas.edu>
2024-09-04 16:27:39 -0500

    commit 2

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <unistd.h>
+
+typedef struct { 
+    char *command;
+    char *args;
+    char *input_file;
+    char *output_file;
+    int append_output;  
+} process;
+
+void parse_input(char *input);
 
 int main() {
-    printf("Hello World!\n");
+
+    while (1) {
+        printf("yash> ");
+        fflush(stdout);
+    }
     return 0;
 }
