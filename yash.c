3aaaad9aef8fccf818e23edb405d2ed64a30bf1e
Albert Wang <albertbw011@utexas.edu>
2024-09-10 00:56:24 -0500

    added piping parsing

--- yash.c
+++ yash.c
@@ -43,6 +43,7 @@ void free_command(Command *cmd) {
 
 /*
 For debugging purposes
+Prints each element of Command object
 */
 void print_command(Command *cmd) {
     if (cmd->command) printf("command: %s\n", cmd->command);
@@ -61,10 +62,9 @@ void execute_command(Command *cmd) {
 
     if (pid == 0) {
         if (handle_redirection(cmd) == 0) {
-            if (execvp(cmd->command, cmd->args) == -1) {
-                printf("\n");
-            }
-        }
+            execvp(cmd->command, cmd->args);
+        } 
+        exit(EXIT_SUCCESS);
     } else {
         waitpid(pid, &status, 0);
     }
@@ -75,6 +75,10 @@ int handle_redirection(Command *cmd) {
 
     if (cmd->input_file != NULL) {
         fd = open(cmd->input_file, O_RDONLY);
+        if (fd < 0) {
+            perror(cmd->input_file);
+            return -1;
+        }
         if (dup2(fd, STDIN_FILENO) < 0) {
             close(fd);
             return -1;
@@ -103,38 +107,34 @@ int handle_redirection(Command *cmd) {
     return 0;
 }
 
+void parse_redirection(Command *cmd, char *token, char **save_ptr) {
+    char *file_name = strtok_r(NULL, DELIM, save_ptr);
+    
+    if (strcmp(token, "<") == 0) 
+        cmd->input_file = strdup(file_name);
+    else if (strcmp(token, ">") == 0) 
+        cmd->output_file = strdup(file_name);
+    else if (strcmp(token, "2>") == 0)
+        cmd->error_file = strdup(file_name);
+}
+
 /*
 Parses input string into 2D character array
 */
 Command* parse_input(char *input) {
     char *token;
     char *save_ptr;
-    int i = 0;
-    int curr_len = 0; 
     int arg_index = 0;
     int args_size = 8;
     Command *cmd = (Command* ) malloc(sizeof(Command));
     init_command(cmd);
 
     cmd->args = (char **) malloc(args_size * sizeof(char *));
-    token = strtok(input, DELIM);
+    token = strtok_r(input, DELIM, &save_ptr);
 
-    while (token != NULL && curr_len < MAX_LINE_CHAR) {
-        curr_len += strlen(token);
-
+    while (token != NULL) {
         if (strcmp(token, ">") == 0 || strcmp(token, "<") == 0 || strcmp(token, "2>") == 0) {
-            char *file_name = strtok(NULL, DELIM);
-    
-            if (file_name != NULL) {
-                if (strcmp(token, "<") == 0) 
-                    cmd->input_file = strdup(file_name);
-                else if (strcmp(token, ">") == 0) {
-                    cmd->output_file = strdup(file_name);
-                } else if (strcmp(token, "2>") == 0)
-                    cmd->error_file = strdup(file_name);
-
-                curr_len += strlen(file_name);
-            }
+            parse_redirection(cmd, token, &save_ptr);
         } else {
             if (cmd->command == NULL)
                 cmd->command = strdup(token);
@@ -143,15 +143,21 @@ Command* parse_input(char *input) {
                 args_size *= 2;
                 cmd->args = (char **) realloc(cmd->args, args_size * sizeof(char *));
             } 
-
             cmd->args[arg_index++] = strdup(token);
         }
 
-        token = strtok(NULL, DELIM);
+        token = strtok_r(NULL, DELIM, &save_ptr);
     }
-
+    
     cmd->args[arg_index] = NULL;
     return cmd;
+}
+
+Command** parse_pipe_input(char *left_command, char *right_command) {
+    Command **commands = (Command **) malloc(2 * sizeof(Command *));
+    commands[0] = parse_input(left_command);
+    commands[1] = parse_input(right_command);
+    return commands;
 }
 
 int main() {
@@ -165,9 +171,21 @@ int main() {
             break;
         } 
 
-        Command *cmd = parse_input(command);
-        execute_command(cmd);
-        free_command(cmd);
+        char *pipe_pos = strstr(command, "|");
+        
+        if (pipe_pos != NULL) {
+            *pipe_pos = '\0';
+            char *left_command = command;
+            char *right_command = pipe_pos + 1;
+            Command **commands = parse_pipe_input(left_command, right_command);
+            free_command(commands[0]);
+            free_command(commands[1]);
+            free(commands);
+        } else {
+            Command *cmd = parse_input(command);
+            execute_command(cmd);
+            free_command(cmd);
+        }
     }
     
     return 0;
